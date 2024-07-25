0bbb068308876f5830bed1203a3c05db2ff73082
Aniket <rayaniket6404@gmail.com>
2024-07-25 09:06:12 +0530

    first

--- script.js
+++ script.js
@@ -95,7 +95,7 @@
 //   // Initial render of movies
 //   renderMovies();
   
-const API_URL = "https://jsday2-rayviniths-projects.vercel.app/initialMovies";
+const API_URL = "http://localhost:3000/initialMovies";
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchMovies();
